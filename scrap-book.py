a5e9972d28a11168a08575c768e4ceb27f342bf2
clmkln <clement.tour39@gmail.com>
2022-04-28 00:05:56 +0200

    Edit class and add new attributes

--- scrap-book.py
+++ scrap-book.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
+from pickle import SHORT_BINUNICODE
 from bs4 import BeautifulSoup
 from pathlib import Path
-import requests, csv, re
+from urllib.parse import urljoin
+import requests, csv, re, os
+
 
 SINGLE_BOOK_URL="http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html"
+SHORTURI=urljoin(SINGLE_BOOK_URL, '/')
+
 
 @dataclass
 class book:
-    category: str
-    #image_url: str
-    number_available: int
-    price_including_tax: float
-    price_excluding_tax: float
-    product_page_url: str
-    product_description: str
-    review_count: int
-    review_rating: int
-    tax:float
-    title: str
-    universal_product_code_upc: str
+    """Class for keeping track of an item in inventory."""
+    category: str = ""
+    image_url: str = ""
+    number_available: int = 0
+    price_including_tax: float = 0.00
+    price_excluding_tax: float = 0.00
+    product_type: str = ""
+    product_page_url: str = ""
+    product_description: str = ""
+    review_count: int = 0
+    review_rating: int = 0
+    tax:float = 0.00
+    title: str = ""
+    universal_product_code_upc: str = ""
 
 def mkdir(folder):
     Path(folder).mkdir(parents=True, exist_ok=True)
 
 def get_book(url):
-    book_info = book(category="",
-                    number_available=0,
-                    price_including_tax=0.00,
-                    price_excluding_tax=0.00,
-                    product_page_url=url,
-                    product_description="",
-                    review_count=0,
-                    review_rating=0,
-                    tax=0.00,
-                    title="",
-                    universal_product_code_upc=""
-                    )
+    book_info = book()
 
     print("Requesting single book...")
     answer = requests.get(url)
@@ -47,9 +43,11 @@ def get_book(url):
     # Extract table
     for element in soup.select(".table-striped"):        
         for cell in element.select("tr"):
-            print(cell)
+            #print(cell)
             if cell.th.text == "UPC":
                 book_info.universal_product_code_upc = cell.td.text
+            elif cell.th.text == "Product Type":
+                book_info.product_type = cell.td.text            
             elif cell.th.text == "Price (excl. tax)":
                 book_info.price_excluding_tax = cell.td.text
             elif cell.th.text == "Price (incl. tax)":
@@ -62,14 +60,16 @@ def get_book(url):
                 book_info.number_available =int("".join(filter(str.isdigit, cell.td.text)))
 
     book_info.title = soup.select(".product_main h1")[0].text
-    book_info.product_description = soup.find_all("p", class_="article") #soup.select("article > p")[0]
+    book_info.product_description = soup.select("article > p")[0].text
+    book_info.image_url = urljoin(SHORTURI, soup.find_all('div', {'class': 'carousel'})[0].find('img')['src'])
 
     print("_______")
-    return book_info
-
+    return book_info.image_url
 
 def main():
     
     print(get_book(SINGLE_BOOK_URL))
+
+    
 
 main()
